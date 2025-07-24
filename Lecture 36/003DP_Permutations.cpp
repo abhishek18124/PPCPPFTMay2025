b8a78374ff91e091be60302a1df802d22e2eddda
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2025-07-24 17:15:25 +0530

    Lecture 36

---
+++ Lecture 36/003DP_Permutations.cpp
@@ -0,0 +1,12 @@
+/*
+
+	https://leetcode.com/problems/permutations/
+
+*/
+
+class Solution {
+public:
+	vector<vector<int>> permute(vector<int>& nums) {
+
+	}
+};
---
+++ Lecture 36/004DP_Matching.cpp
@@ -0,0 +1,17 @@
+/*
+
+	https://atcoder.jp/contests/dp/tasks/dp_o
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	// todo ...
+
+	return 0;
+}
